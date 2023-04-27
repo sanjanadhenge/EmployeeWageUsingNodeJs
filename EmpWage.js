1ddf58603c5e238903f8f69bd3fac0a9965c0ed3
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-27 11:49:33 +0530

    [sanjana] Add :CalculateWage using function

--- EmpWage.js
+++ EmpWage.js
@@ -7,23 +7,24 @@ if (check == Is_present) {
 else {
     console.log("Employee is Absent");
 }
-//UC2
+//UC3
 let Wage_Per_Hr = 20, Full_Day_Hr = 8, Part_Day_Hr = 4,Is_Full_Time=1,Is_Part_Time=2;
 let empHrs = 0;
-
 let check1 =  Math.floor(Math.random()*10) % 3;
-switch(check1)
-{
-    case Is_Full_Time:
-        Is_Full_Time=1;
-         empHrs=Full_Day_Hr;
-         break;
-    case Is_Part_Time:
-        Is_Part_Time=2;
-        empHrs=Part_Day_Hr;
-        break;
-    default:
-        empHrs=0;
-}
+empHrs = GetWorkingHrs(check1);
 let Wage = empHrs*Wage_Per_Hr;
 console.log("Emp Wage "+Wage);
+function GetWorkingHrs(check1)
+{
+    switch(check1)
+    {
+        case Is_Full_Time:
+           return Full_Day_Hr;
+             break;
+        case Is_Part_Time:
+            return Part_Day_Hr;
+            break;
+        default:
+            return 0;
+    }
+}
