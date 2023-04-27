1f7a15a52d29bf411769b623bcd54b1235b75acf
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-27 11:11:18 +0530

    [sanjana] Add :EmpAttendence

---
+++ EmpWage.js
@@ -0,0 +1,9 @@
+//UC1
+let Is_present = 1;
+let check = Math.floor(Math.random()*10%2);
+if (check == Is_present) {
+    console.log("Employee is Present");
+}
+else {
+    console.log("Employee is Absent");
+}
